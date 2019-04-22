0abccd541b98f48609de8962f3e0a8967a969f56
CzepiMM <czepimm200@gmail.com>
2019-04-22 18:38:09 +0200

    Add new properties to Movie class: - Genre - GenreId - ReleaseData - DataAdded - NumberInStock

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +11,14 @@ namespace Vidly.Models
         // POCO - bechaviour
         public int Id { get; set; }
         public string Name { get; set; }
-        
+
+        [Required]
+        public Genre Genre { get; set; }
+        public byte  GenreId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public byte NumberInStock { get; set; }
     }
 
 
