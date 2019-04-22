a2b8ba117898432a7442bd110fd52339e937ac16
CzepiMM <czepimm200@gmail.com>
2019-04-22 18:25:51 +0200

    Add birthdate to a customer: - Add Birthdate to Customer model - Add migration: AddBirthdateToCustomer - Modify table in Customer view

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System;
 
 namespace Vidly.Models
 {
@@ -14,5 +15,6 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? Birthdate { get; set; }
     }
 }
