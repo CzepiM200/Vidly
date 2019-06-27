4d22a034cd70ef99a096ab033b289d859f41e1f3
CzepiMM <czepimm200@gmail.com>
2019-06-28 01:42:14 +0200

    Modify constraints in 'Movie' model

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,14 +10,20 @@ namespace Vidly.Models
     {
         // POCO - bechaviour
         public int Id { get; set; }
-        public string Name { get; set; }
 
         [Required]
+        public string Name { get; set; }
+
         public Genre Genre { get; set; }
+        [Required]
         public byte  GenreId { get; set; }
 
+        [Required]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Required]
         public byte NumberInStock { get; set; }
     }
 
